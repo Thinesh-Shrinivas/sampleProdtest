eb26abf356cbd33eab5540839dc42be4eaea7ef2
Thinesh Shrinivas <shrinivas.thinesh@smartbear.com>
2020-01-15 13:07:42 +0530

    config commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Feedback from './Feedback';
 
-const render = (element) => {
-	ReactDOM.render(<Feedback />, element);
-};
+// const render = (element) => {
+// 	ReactDOM.render(<Feedback />, element);
+// };
 
 export {
-	Feedback, render
+	Feedback
+	// , render
 }
